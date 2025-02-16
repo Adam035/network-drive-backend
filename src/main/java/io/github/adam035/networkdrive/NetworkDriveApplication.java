12585320fc3ea9977b5a653639e8613196b09df1
Adam035 <adamlipian2723@gmail.com>
2025-02-16 23:25:29 +0100

    chore: setup Spring Boot project

---
+++ src/main/java/io/github/adam035/networkdrive/NetworkDriveApplication.java
@@ -0,0 +1,13 @@
+package io.github.adam035.networkdrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkDriveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetworkDriveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/adam035/networkdrive/NetworkDriveApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.adam035.networkdrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkDriveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
